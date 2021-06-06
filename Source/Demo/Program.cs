74d51bc21d39ec21685a634e6983bbdb8d04acb5
Daniel Beus <legacybass.developer@outlook.com>
2021-06-06 09:32:49 -0600

    Added observable based iteration for demo

--- Source/Demo/Program.cs
+++ Source/Demo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading.Tasks;
 
 namespace Demo
@@ -9,27 +11,56 @@ namespace Demo
 		static async Task Main(string[] args)
 		{
 			Console.WriteLine("Fibonacci from AsyncEnumerable");
-			await foreach(int number in Fibonacci(10))
+			await foreach(int number in FibonacciAsync(10))
 			{
 				Console.WriteLine(number);
 			}
+
+			Console.WriteLine("\nFibonacci from Observable");
+
+			var observable = FibonacciObservable(10);
+
+			using var subscription = observable
+			.Subscribe(value => Console.WriteLine(value));
+
+			await observable;
+
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
 		}
 
-		public static async IAsyncEnumerable<int> Fibonacci(int max)
+		public static async IAsyncEnumerable<int> FibonacciAsync(int max)
 		{
-			int current = 1;
-			int previous = 0;
+			int next = 1;
+			int current = 0;
 			int count = 0;
 
-			while(count++ < max)
+			while (count++ < max)
 			{
 				await Task.Delay(200);
-				yield return previous;
+				yield return current;
 
-				int temp = previous;
-				previous = current;
-				current = temp + current;
+				int temp = current;
+				current = next;
+				next = temp + next;
 			}
+		}
+
+		public static IObservable<int> FibonacciObservable(int max)
+		{
+			int current = 0;
+			int next = 1;
+
+			return Observable.Range(0, max)
+			.Select(count =>
+			{
+				int value = current;
+
+				current = next;
+				next = value + current;
+
+				return value;
+			});
 		}
 	}
 }
