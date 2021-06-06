6da66162d542816790509a08f34e1ec5ba1864a2
Daniel Beus <legacybass.developer@outlook.com>
2021-06-06 08:56:43 -0600

    Added fibonacci with AsyncEnumerable

--- Source/Demo/Program.cs
+++ Source/Demo/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Demo
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Fibonacci from AsyncEnumerable");
+			await foreach(int number in Fibonacci(10))
+			{
+				Console.WriteLine(number);
+			}
+		}
+
+		public static async IAsyncEnumerable<int> Fibonacci(int max)
+		{
+			int current = 1;
+			int previous = 0;
+			int count = 0;
+
+			while(count++ < max)
+			{
+				await Task.Delay(200);
+				yield return previous;
+
+				int temp = previous;
+				previous = current;
+				current = temp + current;
+			}
 		}
 	}
 }
